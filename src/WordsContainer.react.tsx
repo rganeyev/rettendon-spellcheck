9faab316b2446b2d32a27eb8af5f4aaed3871b27
Rustam Ganeyev <rganeyev@gmail.com>
2024-10-27 11:19:52 +0000

    Update WordsContainer.react.tsx
    Added words for year 2

--- src/WordsContainer.react.tsx
+++ src/WordsContainer.react.tsx
@@ -4,26 +4,34 @@ import Button from "@mui/material/Button";
 import { useCallback, useState } from "react";
 import "./App.css";
 
+// Year 4 words
+// const words = [
+//     "accident", "accidentally", "actually", "actual", "address", "although", "answer", "appear", "arrive",
+//     "believe", "bicycle", "breath", "breathe", "build", "busy", "business",
+//     "calendar", "caught", "centre", "century", "certain", "circle", "consider", "continue",
+//     "decide", "describe", "different", "difficult", "disappear",
+//     "early", "earth", "eight", "eighth", "enough", "entrance", "exercise", "experience", "experiment", "extreme",
+//     "famous", "favourite", "february", "forward", "fruit", "grammar", "group", "guard", "guide",
+//     "heard", "heart", "height", "history",
+//     "imagine", "important", "increase", "interest", "island",
+//     "knowledge",
+//     "learn", "length", "library",
+//     "material", "medicine", "mention", "minute",
+//     "natural", "naughty", "notice",
+//     "occasion", "occasionally", "often", "opposite", "oridnary",
+//     "pearl", "particular", "peculiar", "perhaps", "popular", "position", "possess", "possession", "possible", "potatoes", "pressure", "probably", "promise", "purpose",
+//     "quarter", "question",
+//     "recent", "regular", "reign", "remember",
+//     "sentence", "separate", "special", "straight", "strange", "strength", "suppose", "surprise",
+//     "therefore", "though", "thought", "through",
+//     "various", "weight", "woman", "women"
+// ];
+
+// Year 1 words
 const words = [
-    "accident", "accidentally", "actually", "actual", "address", "although", "answer", "appear", "arrive",
-    "believe", "bicycle", "breath", "breathe", "build", "busy", "business",
-    "calendar", "caught", "centre", "century", "certain", "circle", "consider", "continue",
-    "decide", "describe", "different", "difficult", "disappear",
-    "early", "earth", "eight", "eighth", "enough", "entrance", "exercise", "experience", "experiment", "extreme",
-    "famous", "favourite", "february", "forward", "fruit", "grammar", "group", "guard", "guide",
-    "heard", "heart", "height", "history",
-    "imagine", "important", "increase", "interest", "island",
-    "knowledge",
-    "learn", "length", "library",
-    "material", "medicine", "mention", "minute",
-    "natural", "naughty", "notice",
-    "occasion", "occasionally", "often", "opposite", "oridnary",
-    "pearl", "particular", "peculiar", "perhaps", "popular", "position", "possess", "possession", "possible", "potatoes", "pressure", "probably", "promise", "purpose",
-    "quarter", "question",
-    "recent", "regular", "reign", "remember",
-    "sentence", "separate", "special", "straight", "strange", "strength", "suppose", "surprise",
-    "therefore", "though", "thought", "through",
-    "various", "weight", "woman", "women"
+    "the", "a", "do", "today", "of", "said", "says", "are", "were", "was", "is", "his", "has", "I",
+    "we", "no", "go", "so", "by", "my", "here", "there", "where", "love", "come", "some", "one", "once", "ask",
+    "pull", "full", "he", "me", "she", "house", "our", "friend", "school", "put", "push", "you", "your", "they", "be"
 ];
 
 function getRandomWord(): string {
